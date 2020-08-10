eb0e72dbc9f2753f46aeed81564834d298ae0a52
Vikas Munukutla <vmunukutla@google.com>
2020-08-10 09:09:20 +0000

    commented GRPCStreamDemuxer extension code, added TODOs to explain upcoming design changes

--- source/extensions/grpc_stream_demuxer/demuxer.cc
+++ source/extensions/grpc_stream_demuxer/demuxer.cc
@@ -7,6 +7,10 @@ GRPCStreamDemuxer::GRPCStreamDemuxer(std::string subscription, std::string addre
     : subscription_(subscription), address_(address), port_(port) {}
 
 void GRPCStreamDemuxer::start() {
+    // TODO (vmunukutla): Implement so that it forms a streaming pull connection
+    // to subscription_, waits for messages to be published to subscription_, and
+    // sends unary grpc requests to address:port when messages are published to
+    // subscription_.
     std::cout << "Subscription: " << subscription_ << std::endl;
     std::cout << "Address: " << address_ << std::endl;
     std::cout << "Port: " << port_ << std::endl;
