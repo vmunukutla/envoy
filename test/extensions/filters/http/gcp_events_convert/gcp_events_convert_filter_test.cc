d9219139f983d55e8d268e0182453e7ffaa34448
Hongxiang Jiang <hxjiang@google.com>
2020-08-11 05:06:44 +0000

    update proto file which will now have content_type
    1. implement the basic logic regarding the filter.
    	a. decode header will now determine whether to trigger the
    	functionality
    	b. decode data will have basic logic
    2. updae config class
    3. update test files[WIP]
    4. rebase with the current master

--- test/extensions/filters/http/gcp_events_convert/gcp_events_convert_filter_test.cc
+++ test/extensions/filters/http/gcp_events_convert/gcp_events_convert_filter_test.cc
@@ -4,7 +4,10 @@
 #include "envoy/extensions/filters/http/gcp_events_convert/v3/gcp_events_convert.pb.h"
 
 #include "common/http/header_map_impl.h"
+#include "common/protobuf/utility.h"
 #include "common/runtime/runtime_impl.h"
+
+#include "google/pubsub/v1/pubsub.pb.h"
 
 #include "extensions/filters/http/gcp_events_convert/gcp_events_convert_filter.h"
 #include "extensions/filters/http/well_known_names.h"
@@ -22,82 +25,82 @@ namespace Extensions {
 namespace HttpFilters {
 namespace GcpEventsConvert {
 
-TEST(GcpEventsConvertFilterUnitTest, DecodeHeaderTestWithLowerCases) {
+using google::pubsub::v1::PubsubMessage;
+using google::pubsub::v1::ReceivedMessage;
+
+// Unit test for Decode Headers
+TEST(GcpEventsConvertFilterUnitTest, DecoderHeaderWithCloudEventBody) {
   envoy::extensions::filters::http::gcp_events_convert::v3::GcpEventsConvert config;
-  config.set_key("some random key");
-  config.set_val("some random value");
+  config.set_content_type("application/grpc+cloudevent+json");
   GcpEventsConvertFilter filter(std::make_shared<GcpEventsConvertFilterConfig>(config));
-  Http::TestRequestHeaderMapImpl headers;
+  Http::TestRequestHeaderMapImpl headers({{"content-type", "application/grpc+cloudevent+json"},
+                                          {"content-length", "100"}});
+
+  EXPECT_EQ(Http::FilterHeadersStatus::StopIteration, filter.decodeHeaders(headers, false));
+}
+
+TEST(GcpEventsConvertFilterUnitTest, DecodeHeaderWithCloudEventNoBody) {
+  envoy::extensions::filters::http::gcp_events_convert::v3::GcpEventsConvert config;
+  config.set_content_type("application/grpc+cloudevent+json");
+  GcpEventsConvertFilter filter(std::make_shared<GcpEventsConvertFilterConfig>(config));
+  Http::TestRequestHeaderMapImpl headers({{"content-type", "application/grpc+cloudevent+json"},
+                                          {"content-length", "0"}});
 
   EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter.decodeHeaders(headers, true));
-  const Http::HeaderEntry* entry = headers.get(Http::LowerCaseString("some random key"));
-  ASSERT_THAT(entry, testing::NotNull());
-  EXPECT_EQ(entry->value() , "some random value");
 }
 
-TEST(GcpEventsConvertFilterUnitTest, DecodeHeaderTestWithUpperCaseKey) {
+TEST(GcpEventsConvertFilterUnitTest, DecodeHeaderWithRandomContent) {
   envoy::extensions::filters::http::gcp_events_convert::v3::GcpEventsConvert config;
-  config.set_key("SOME RANDOM KEY");
-  config.set_val("some random value");
+  config.set_content_type("application/grpc+cloudevent+json");
   GcpEventsConvertFilter filter(std::make_shared<GcpEventsConvertFilterConfig>(config));
-  Http::TestRequestHeaderMapImpl headers;
+  Http::TestRequestHeaderMapImpl headers({{"content-type", "application/json"},
+                                          {"content-length", "100"}});
 
-  EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter.decodeHeaders(headers, true));
-  const Http::HeaderEntry* entry = headers.get(Http::LowerCaseString("some random key"));
-  ASSERT_THAT(entry, testing::NotNull());
-  EXPECT_EQ(entry->value() , "some random value");
+  EXPECT_EQ(Http::FilterHeadersStatus::Continue, filter.decodeHeaders(headers, false));
 }
 
-TEST(GcpEventsConvertFilterUnitTest, DecodeDataTestWithOneBody) {
+// Unit test for Decode Data
+TEST(GcpEventsConvertFilterUnitTest, DecodeDataWithCloudEvent) {
   envoy::extensions::filters::http::gcp_events_convert::v3::GcpEventsConvert config;
-  config.set_key("some random key");
-  config.set_val("some random value");
+  config.set_content_type("application/grpc+cloudevent+json");
   GcpEventsConvertFilter filter(std::make_shared<GcpEventsConvertFilterConfig>(config));
+  Http::TestRequestHeaderMapImpl headers({{"content-type", "application/grpc+cloudevent+json"}});
 
-  Buffer::OwnedImpl data1("hello");
-  EXPECT_EQ(Http::FilterDataStatus::Continue, filter.decodeData(data1, false));
+  EXPECT_EQ(Http::FilterHeadersStatus::StopIteration, filter.decodeHeaders(headers, false));
 
-  Buffer::OwnedImpl data2;
-  EXPECT_EQ(Http::FilterDataStatus::Continue, filter.decodeData(data1, true));
-}
+  // create a received message proto object
+  ReceivedMessage received_message;
+  received_message.set_ack_id("random ack id");
+  received_message.set_delivery_attempt(3);
+  PubsubMessage pubsub_message = *received_message.mutable_message();
+  auto attributes = *pubsub_message.mutable_attributes();
+  attributes["ce-specversion"] = "1.0";
+  attributes["ce-type"] = "com.example.some_event";
+  attributes["ce-time"] = "2020-03-10T03:56:24Z";
+  attributes["ce-id"] = "1234-1234-1234";
+  attributes["ce-source"] = "/mycontext/subcontext";
+  attributes["ce-datacontenttype"] = "application/text; charset=utf-8";
 
-TEST(GcpEventsConvertFilterUnitTest, DecodeDataTestWithMultipleBody) {
-  envoy::extensions::filters::http::gcp_events_convert::v3::GcpEventsConvert config;
-  config.set_key("some random key");
-  config.set_val("some random value");
-  GcpEventsConvertFilter filter(std::make_shared<GcpEventsConvertFilterConfig>(config));
-
-  Buffer::OwnedImpl data1("hello");
-  EXPECT_EQ(Http::FilterDataStatus::Continue, filter.decodeData(data1, false));
-
-  Buffer::OwnedImpl data2(" world");
-  EXPECT_EQ(Http::FilterDataStatus::Continue, filter.decodeData(data2, false));
-
-  Buffer::OwnedImpl data3("! ");
-  EXPECT_EQ(Http::FilterDataStatus::Continue, filter.decodeData(data3, false));
-
-  Buffer::OwnedImpl data4;
-  EXPECT_EQ(Http::FilterDataStatus::Continue, filter.decodeData(data4, true));
-}
-
-TEST(GcpEventsConvertFilterUnitTest, DecodeDataTestWithoutBody) {
-  envoy::extensions::filters::http::gcp_events_convert::v3::GcpEventsConvert config;
-  config.set_key("some random key");
-  config.set_val("some random value");
-  GcpEventsConvertFilter filter(std::make_shared<GcpEventsConvertFilterConfig>(config));
-
-  Buffer::OwnedImpl data;
-  EXPECT_EQ(Http::FilterDataStatus::Continue, filter.decodeData(data, true));
-}
-
-TEST(GcpEventsConvertFilterUnitTest, DecodeTrailerTest) {
-  envoy::extensions::filters::http::gcp_events_convert::v3::GcpEventsConvert config;
-  config.set_key("some random key");
-  config.set_val("some random value");
-  GcpEventsConvertFilter filter(std::make_shared<GcpEventsConvertFilterConfig>(config));
-
-  Http::TestRequestTrailerMapImpl trailer;
-  EXPECT_EQ(Http::FilterTrailersStatus::Continue, filter.decodeTrailers(trailer));
+  pubsub_message.set_data("cloud event data payload");
+  pubsub_message.set_message_id("136969346945");
+  pubsub_message.mutable_publish_time()->ParseFromString("2014-10-02T15:01:23Z");
+  pubsub_message.set_ordering_key("");
+  
+  std::string json_string;
+  auto status = Envoy::ProtobufUtil::MessageToJsonString(received_message , &json_string);
+  ASSERT_TRUE(status.ok());
+  std::cout << "-1" << std::endl;
+  for (size_t index = 0; index < json_string.size(); index += 10) {
+    size_t length = (json_string.size() - index) < 10 ? (json_string.size() - index) : 10;
+    Buffer::OwnedImpl data(json_string.substr(index , length));
+    EXPECT_EQ(Http::FilterDataStatus::StopIterationAndBuffer, 
+              filter.decodeData(data, false));
+  }
+  std::cout << "-2" << std::endl;
+  Buffer::OwnedImpl data(" ");
+  std::cout << "-3" << std::endl;
+  EXPECT_EQ(Http::FilterDataStatus::Continue, 
+            filter.decodeData(data, true));
 }
 
 } // namespace GcpEventsConvert
