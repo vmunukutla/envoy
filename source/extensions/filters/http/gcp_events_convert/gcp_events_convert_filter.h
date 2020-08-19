fc17950ddd329d74fd786b1af8de42a5c5aa1136
Hongxiang Jiang <hxjiang@google.com>
2020-08-19 18:20:48 +0000

    replace typedef with using
    1. it's recommended to use using instead of typedef in style guide.
    2. replace using declaration inside class for readablity

--- source/extensions/filters/http/gcp_events_convert/gcp_events_convert_filter.h
+++ source/extensions/filters/http/gcp_events_convert/gcp_events_convert_filter.h
@@ -14,8 +14,6 @@ namespace Extensions {
 namespace HttpFilters {
 namespace GcpEventsConvert {
 
-typedef boost::beast::http::request<boost::beast::http::string_body> HttpRequest;
-
 struct GcpEventsConvertFilterConfig : public Router::RouteSpecificFilterConfig {
   GcpEventsConvertFilterConfig(
       const envoy::extensions::filters::http::gcp_events_convert::v3::GcpEventsConvert&
@@ -47,6 +45,8 @@ public:
   void setDecoderFilterCallbacks(Http::StreamDecoderFilterCallbacks& callbacks) override;
 
 private:
+  using HttpRequest = boost::beast::http::request<boost::beast::http::string_body>;
+  
   bool isCloudEvent(const Http::RequestHeaderMap& headers) const;
 
   // modify the data of HTTP request
