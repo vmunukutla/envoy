e050d7176d0d233b9e3926a84c3524bc3dd331ae
Vikas Munukutla <vmunukutla@google.com>
2020-09-02 21:09:59 +0000

    modified grpc_stream_demuxer to implement sending unary request

--- source/extensions/grpc_stream_demuxer/grpc_stream_demuxer.cc
+++ source/extensions/grpc_stream_demuxer/grpc_stream_demuxer.cc
@@ -1,5 +1,13 @@
 #include "extensions/grpc_stream_demuxer/grpc_stream_demuxer.h"
 
+#include "common/protobuf/protobuf.h"
+
+using grpc::ClientContext;
+using grpc::ClientReaderWriter;
+using google::pubsub::v1::Subscriber;
+using google::pubsub::v1::StreamingPullRequest;
+using google::pubsub::v1::StreamingPullResponse;
+
 namespace Envoy {
 namespace Extensions {
 namespace GrpcStreamDemuxer {
@@ -42,6 +50,11 @@ void GrpcStreamDemuxer::start() {
     for (const auto &message : response.received_messages()) {
       // Print the data from the message.
       ENVOY_LOG(info, "Pubsub message data: {}", message.message().data());
+      // Send the message using a unary grpc request.
+      std::string target_uri = address_ + ":" + std::to_string(port_);
+      ReceivedMessageServiceClient client(grpc::CreateChannel(target_uri, grpc::InsecureChannelCredentials()));
+      std::string reply = client.SendReceivedMessage(message);
+      ENVOY_LOG(info, "Unary request response: {}", reply);
       ack_request.add_ack_ids(message.ack_id());
     }
     stream->Write(ack_request);
