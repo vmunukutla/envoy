a075aff9de124ee8864654e6e905664aee6557ed
Vikas Munukutla <vmunukutla@google.com>
2020-09-07 19:21:10 +0000

    implemented feedback from PR

--- test/extensions/grpc_stream_demuxer/grpc_stream_demuxer_test.cc
+++ test/extensions/grpc_stream_demuxer/grpc_stream_demuxer_test.cc
@@ -11,31 +11,6 @@ namespace Envoy {
 namespace Extensions {
 namespace GrpcStreamDemuxer {
 
-// Basic test to make sure GrpcStreamDemuxer proto initialization from yaml config is successful.
-TEST(GrpcStreamDemuxerTest, CreateGrpcStreamDemuxerProto) {
-  const std::string yaml = R"EOF(
-  subscription: "test_subscription_0"
-  address: 0.0.0.0
-  port: 10000
-  )EOF";
-  envoy::extensions::grpc_stream_demuxer::v3alpha::GrpcStreamDemuxer demuxer_object;
-  TestUtility::loadFromYaml(yaml, demuxer_object);
-  EXPECT_EQ(demuxer_object.subscription(), "test_subscription_0");
-  EXPECT_EQ(demuxer_object.address(), "0.0.0.0");
-  EXPECT_EQ(demuxer_object.port(), 10000);
-}
-
-// Basic test to make sure GrpcStreamDemuxer proto initialization from invalid yaml config fails.
-TEST(GrpcStreamDemuxerTest, InvalidGrpcStreamDemuxerProto) {
-  const std::string yaml = R"EOF(
-  invalid_field: "test_subscription_0"
-  address: 0.0.0.0
-  port: 10000
-  )EOF";
-  envoy::extensions::grpc_stream_demuxer::v3alpha::GrpcStreamDemuxer demuxer_object;
-  EXPECT_THROW(TestUtility::loadFromYaml(yaml, demuxer_object), EnvoyException);
-}
-
 // Basic test to make sure GrpcStreamDemuxer initialization from yaml config is successful.
 TEST(GrpcStreamDemuxerTest, CreateGrpcStreamDemuxer) {
   const std::string yaml = R"EOF(
@@ -43,17 +18,11 @@ TEST(GrpcStreamDemuxerTest, CreateGrpcStreamDemuxer) {
   address: 0.0.0.1
   port: 10001
   )EOF";
-  envoy::extensions::grpc_stream_demuxer::v3alpha::GrpcStreamDemuxer demuxer_object;
-  TestUtility::loadFromYaml(yaml, demuxer_object);
+  envoy::extensions::grpc_stream_demuxer::v3alpha::GrpcStreamDemuxer demuxer_config;
+  TestUtility::loadFromYaml(yaml, demuxer_config);
   auto& factory = Config::Utility::getAndCheckFactoryByName<GrpcStreamDemuxerFactory>("grpc_stream_demuxer");
-  GrpcStreamDemuxerPtr demuxer = factory.createGrpcStreamDemuxer(demuxer_object);
+  GrpcStreamDemuxerPtr demuxer = factory.createGrpcStreamDemuxer(demuxer_config);
   EXPECT_THAT(demuxer, testing::NotNull());
-}
-
-// Basic test to make sure GrpcStreamDemuxer initialization from an invalid factory name fails.
-TEST(GrpcStreamDemuxerTest, InvalidGrpcStreamDemuxerFactory) {
-  EXPECT_THROW(Config::Utility::getAndCheckFactoryByName<GrpcStreamDemuxerFactory>("invalid_factory_name"), 
-    EnvoyException);
 }
 
 } // namespace GrpcStreamDemuxer
